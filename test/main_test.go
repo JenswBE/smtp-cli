051823f06045f72f1524835e6ac70893564c1158
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2022-05-16 20:50:18 +0200

    Execute test server comaand + Improve logging

--- test/main_test.go
+++ test/main_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -20,7 +21,14 @@ func Test_E2ETestSuite(t *testing.T) {
 }
 
 func (s *E2ETestSuite) SetupSuite() {
-	exec.Command("docker", "compose", "up", "-d")
+	// Start SMTP mock server
+	log.Info().Msg("Starting SMTP mock server ...")
+	cmd := exec.Command("docker", "compose", "up", "-d")
+	result, err := cmd.CombinedOutput()
+	require.NoErrorf(s.T(), err, "Failed to start Docker Compose: %s", string(result))
+
+	// Poll SMTP mock server for completed start
+	log.Info().Msg("Polling SMTP mock server ...")
 	for i := 0; true; i++ {
 		_, err := getMessages()
 		if err == nil {
@@ -33,9 +41,11 @@ func (s *E2ETestSuite) SetupSuite() {
 				require.FailNow(s.T(), "Unable to contact SMTP mock server after 20 seconds")
 			}
 			// Retry in 2 seconds
+			log.Info().Msg("Polling SMTP mock server failed, retrying in 2 seconds")
 			time.Sleep(2 * time.Second)
 		}
 	}
+	log.Info().Msg("SMTP mock server up and running")
 }
 
 func (s *E2ETestSuite) SetupTest() {
